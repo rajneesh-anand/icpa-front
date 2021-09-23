252db66c0caeb549569aec02d60c40143dc91521
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-09-23 17:43:21 +0530

    singin-prerender-error 8

--- src/libs/prisma.js
+++ src/libs/prisma.js
@@ -1,13 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-// let prisma: PrismaClient;
+let prisma;
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
     global.prisma = new PrismaClient();
   }
+
   prisma = global.prisma;
 }
 

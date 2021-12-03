2780e3d314eb8dea1f079c0e8e6ceca57f936e41
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-12-03 15:42:54 +0530

    chapters api changes two

--- src/components/Course/course-details/index.js
+++ src/components/Course/course-details/index.js
@@ -44,7 +44,7 @@ const CourseDetail = ({ data }) => {
       `${process.env.PUBLIC_URL}/api/chapters/${data.slug}`
     );
     const result = await res.json();
-    console.log(result);
+    // console.log(result);
     setChapters(result.data);
   }, []);
 

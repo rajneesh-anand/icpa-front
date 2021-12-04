29462204c7a39cb1ba1460768c0cc847cf13019e
rajneesh.k.anand@gmail.com <rajneesh.k.anand@gmail.com>
2021-12-04 16:21:05 +0530

    aos removed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import { Provider } from "next-auth/client";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import * as ga from "@/libs/ga";
-import AOS from "aos";
 
 import "@/styles/scss/styles.scss";
 
@@ -10,14 +9,6 @@ const App = ({ Component, pageProps }) => {
   const router = useRouter();
 
   useEffect(() => {
-    AOS.init({
-      offset: 80,
-      duration: 1000,
-      once: true,
-      easing: "ease",
-    });
-    AOS.refresh();
-
     const handleRouteChange = (url) => {
       ga.pageview(url);
     };
